13abd7811c71bcbf7144e35a449720d8fd3d4e01
kimzeze <fridaynight@kakao.com>
2023-03-13 20:24:36 +0900

    [fix] Link 컴포넌트 애드온 구성

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,22 +1,12 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import { Link as ReactRouterLink } from 'react-router-dom';
 import './link.css';
-import { Link } from 'react-router-dom';
 
-/**
- * Primary UI component for user interaction
- */
-
-export const LinkC = ({ path, label }) => {
-  return <Link to={path}>{label}</Link>;
+export const Link = (props) => {
+  return <ReactRouterLink to={props.path}>{props.label}</ReactRouterLink>;
 };
 
-LinkC.propTypes = {
-  path: PropTypes.string.isRequired,
+Link.propTypes = {
+  path: PropTypes.number.isRequired,
   label: PropTypes.string.isRequired,
-};
-
-LinkC.defaultProps = {
-  path: '/',
-  label: '메인페이지로 가기',
 };
--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -1,19 +1,24 @@
-import { LinkC } from './Link';
+// 애드온 패키지로부터 withRouter 고차 함수를 추출해 글로벌 decorators, parameters를 설정
+// import { withRouter } from 'storybook-addon-react-router-v6';
+// export const decorators = [withRouter];
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+// export const parameters = {
+//   reactRouter: {
+//     routePath: '/',
+//   },
+// };
+
+import { Link } from './Link';
+
 export default {
   title: 'Example/Link',
-  component: LinkC,
+  component: Link,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const mainLink = {
+export const HomeLink = {
   args: {
-    path: '/',
-    label: 'test',
+    to: '/',
+    label: '홈 페이지로 돌아가기',
   },
 };
