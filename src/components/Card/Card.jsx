16d7ded3da90e49b962ca9653bb17675fcd4cace
kimzeze <fridaynight@kakao.com>
2023-03-16 16:22:43 +0900

    [feat] card 컴포넌트 마크업 및 기초 CSS 완성

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,53 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './card.css';
+import './reset.css';
 
-export const Card = ({ className }) => {
+import { Button } from '../Button/Button';
+import { Title } from '../Title/Title';
+
+export const Card = (props) => {
   return (
-    <>
-      <Swiper
-        className={className}
-        slidesPerView={slidesPerView}
-        spaceBetween={spaceBetween}
-        centeredSlides={centeredSlides}
-        autoplay={autoplay}
-        pagination={pagination}
-        loop={loop}
-        modules={[Autoplay, Pagination, Navigation]}
-        navigation
+    <div
+      className="card__container"
+      style={{
+        width: props.containerWidth,
+        height: props.containerHeight,
+        gap: props.componentGap,
+      }}
+    >
+      <img
+        className="product__image"
+        src={props.src}
+        alt={props.alt}
+        width={props.imageWidth}
+        height={props.imageHeight}
+      />
+      <div
+        className="content"
+        style={{ width: props.contentWidth, height: props.contentHeight }}
       >
-        <SwiperSlide>
-          {context}
-          <img src={src} alt={alt} />
-        </SwiperSlide>
-        <SwiperSlide>
-          {context}
-          <img src={src} alt={alt} />
-        </SwiperSlide>
-        <SwiperSlide>
-          {context}
-          <img src={src} alt={alt} />
-        </SwiperSlide>
-        <SwiperSlide>
-          {context}
-          <img src={src} alt={alt} />
-        </SwiperSlide>
-        <SwiperSlide>
-          {context}
-          <img src={src} alt={alt} />
-        </SwiperSlide>
-      </Swiper>
-    </>
+        <p className="category__title">{props.categoryTitle}</p>
+        <h3 className="product__title">{props.productTitle}</h3>
+        <div className="review">
+          <span>{props.score}/5</span>
+          <span> | </span>
+          <span>{props.reviewsNumber}</span>
+          <span>Reviews</span>
+        </div>
+        <span className="newItem">{props.newItem}</span>
+        <div className="price">
+          <span className="prevPrice">{props.prevPrice}원 /</span>
+          <span className="currentPrice">{props.currentPrice}원</span>
+        </div>
+        <Button label={'장바구니'}></Button>
+      </div>
+    </div>
   );
 };
 
-Carousel.propTypes = {
-  className: PropTypes.string.isRequired,
-  slidesPerView: PropTypes.number.isRequired,
-  spaceBetween: PropTypes.number,
-  pagination: PropTypes.object,
-  centeredSlides: PropTypes.bool,
-  autoplay: PropTypes.object,
-  loop: PropTypes.bool,
-  src: PropTypes.string.isRequired,
-};
+// Card.propTypes = {
+//   className: PropTypes.string.isRequired,
+// };
