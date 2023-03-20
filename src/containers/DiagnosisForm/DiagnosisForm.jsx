b644600bf3313b70f740473b013a4cb53ea625f4
bellaru2022 <rkddls0918@naver.com>
2023-03-20 22:41:21 +0900

    [design] css 수정중

--- src/containers/DiagnosisForm/DiagnosisForm.jsx
+++ src/containers/DiagnosisForm/DiagnosisForm.jsx
@@ -56,3 +56,6 @@ export const SimpleAccordion = ({
     </Accordion>
   );
 };
+
+
+
--- src/pages/Diagnosis/Diagnosis.jsx
+++ src/pages/Diagnosis/Diagnosis.jsx
@@ -4,55 +4,15 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import './diagnosis.css';
-import MonsteraIcon from '../../assets/images/monstera_icon.svg';
-import DownArrow from '../../assets/images/down_arrow.svg';
+import { Title } from '../../components/Title/Title';
+import { DiagnosisForm } from '../../containers/DiagnosisForm/DiagnosisForm';
 
-export const SimpleAccordion = ({
-  title = 'Accordion',
-  contents,
-  defaultExpanded,
-  disabled,
-  onChange,
-  square,
-  expanded,
-}) => {
+
+export const Diagnosis = () => {
   return (
-    <Accordion
-      defaultExpanded={defaultExpanded}
-      disabled={disabled ?? false}
-      onChange={onChange}
-      square={square ?? false}
-      expanded={expanded}
-      disableGutters={true}
-    >
-      <AccordionSummary
-        className="accordion--summary"
-        aria-controls="panel1a-content"
-        expandIcon={<img src={DownArrow} alt="Expand" />}
-      >
-        <Typography component="div" className="accordion--title">
-          <img src={MonsteraIcon} alt="Leaf" />
-          {title}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails className="accordion--detail">
-        {contents.map((content) => {
-          return (
-            <div className="accordion--detail--content--wrapper">
-              <Typography className="accordion--detail--title">
-                {content.title}
-              </Typography>
-              {content.description.map((desc) => {
-                return (
-                  <Typography className="accordion--detail--desc">
-                    {desc}
-                  </Typography>
-                );
-              })}
-            </div>
-          );
-        })}
-      </AccordionDetails>
-    </Accordion>
+    <section className="register_containerss">
+      <Title type={1} label="식물진단 가이드" />
+      <DiagnosisForm />
+    </section>
   );
 };
